51f31564017ad2d792e0f9c624e0ea07a6f8e680
Achyutmanas Tiwari <ammt501@gmail.com>
2022-11-17 12:29:07 +0530

    logic added to operators

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,16 +1,19 @@
-const getHistory = () => {
+function getHistory() {
   return document.getElementById("history-value").innerText;
-};
+}
 
 function printHistory(num) {
   document.getElementById("history-value").innerText = num;
 }
 
-const getOutput = () => {
+function getOutput() {
   return document.getElementById("output-value").innerText;
-};
+}
 
 function printOutput(num) {
+  if (num == "-") {
+    return "";
+  }
   if (num == "") {
     document.getElementById("output-value").innerText = num;
   } else {
@@ -34,12 +37,31 @@ for (var i = 0; i < operator.length; i++) {
     if (this.id == "clear") {
       printHistory("");
       printOutput("");
-    }
-
-    if (this.id == "backspace") {
+    } else if (this.id == "backspace") {
       var output = reverseNumberFormat(getOutput()).toString();
       if (output) {
         printOutput(output.substring(0, output.length - 1));
+      }
+    } else {
+      var output = getOutput();
+      var history = getHistory();
+      if (output == "" && history != "") {
+        if (isNaN(history[history.length - 1])) {
+          history = history.substring(0, history.length - 1);
+        }
+      }
+      if (output != "" || history != "") {
+        output = output == "" ? output : reverseNumberFormat(output);
+        history = history + output;
+        if (this.id == "=") {
+          var result = eval(history);
+          printOutput(result);
+          printHistory("");
+        } else {
+          history = history + this.id;
+          printHistory(history);
+          printOutput("");
+        }
       }
     }
   });
