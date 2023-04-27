8df0458ee5ee889346089316389e58d4d3efdc9c
Achyutmanas Tiwari <ammt501@gmail.com>
2023-04-27 18:21:51 +0530

    word guessing game

---
+++ Word Guessing Game/js/words.js
@@ -0,0 +1,114 @@
+let wordList = [
+  {
+    word: "english",
+    hint: "Hello, world!",
+  },
+  {
+    word: "french",
+    hint: "Bonjour le monde!",
+  },
+  {
+    word: "german",
+    hint: "Hallo Welt!",
+  },
+  {
+    word: "italian",
+    hint: "Ciao, mondo!",
+  },
+  {
+    word: "japanese",
+    hint: "こんにちは、世界！",
+  },
+  {
+    word: "korean",
+    hint: "안녕, 세상아!",
+  },
+  {
+    word: "mandarin",
+    hint: "你好，世界！",
+  },
+  {
+    word: "russian",
+    hint: "Привет, мир!",
+  },
+  {
+    word: "spanish",
+    hint: "¡Hola, mundo!",
+  },
+  {
+    word: "arabic",
+    hint: "مرحبا، العالم!",
+  },
+  {
+    word: "bengali",
+    hint: "হ্যালো, বিশ্ব!",
+  },
+  {
+    word: "dutch",
+    hint: "Hallo, wereld!",
+  },
+  {
+    word: "greek",
+    hint: "Γεια σου, κόσμε!",
+  },
+  {
+    word: "hebrew",
+    hint: "שלום, עולם!",
+  },
+  {
+    word: "hindi",
+    hint: "नमस्ते, दुनिया!",
+  },
+  {
+    word: "indonesian",
+    hint: "Halo, dunia!",
+  },
+  {
+    word: "portuguese",
+    hint: "Olá, mundo!",
+  },
+  {
+    word: "swedish",
+    hint: "Hej, världen!",
+  },
+  {
+    word: "turkish",
+    hint: "Merhaba, dünya!",
+  },
+  {
+    word: "odia",
+    hint: "ନମସ୍କାର, ବିଶ୍ଵ!",
+  },
+  {
+    word: "punjabi",
+    hint: "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ, ਦੁਨਿਆ!",
+  },
+  {
+    word: "tamil",
+    hint: "வணக்கம், உலகம்!",
+  },
+  {
+    word: "telugu",
+    hint: "హలో, ప్రపంచం!",
+  },
+  {
+    word: "urdu",
+    hint: "سلام دنیا!",
+  },
+  {
+    word: "sanskrit",
+    hint: "नमस्कारः जगत्!",
+  },
+  {
+    word: "sindhi",
+    hint: "هيلو، عالم!",
+  },
+  {
+    word: "manipuri",
+    hint: "হাই, লোক!",
+  },
+  {
+    word: "kashmiri",
+    hint: "آسلام علیکم، دنیا!",
+  },
+];
